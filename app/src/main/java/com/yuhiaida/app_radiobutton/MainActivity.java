a9f958dfe4c2060cad29b6c255f7cc82638eb14e
Bernardo <YuhiAida@gmail.com>
2020-02-17 02:45:15 -0300

    Agregada interface

--- app/src/main/java/com/yuhiaida/app_radiobutton/MainActivity.java
+++ app/src/main/java/com/yuhiaida/app_radiobutton/MainActivity.java
@@ -3,12 +3,25 @@ package com.yuhiaida.app_radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        private EditText et1, et2;
+        private TextView tvResultado;
+        private RadioButton rb1, rb2;
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
