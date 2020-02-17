c7eb1316258f36cb4dde9ec98841462fa48c7e3c
Bernardo <YuhiAida@gmail.com>
2020-02-17 03:47:26 -0300

    Agregada multiplicacion y division

--- app/src/main/java/com/yuhiaida/app_radiobutton/MainActivity.java
+++ app/src/main/java/com/yuhiaida/app_radiobutton/MainActivity.java
@@ -7,12 +7,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et1, et2;
     private TextView tvResultado;
-    private RadioButton rb1, rb2;
+    private RadioButton rb1, rb2, rb3, rb4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
         et2 = (EditText)findViewById(R.id.editText2);
         tvResultado = (TextView)findViewById(R.id.textView);
         rb1 = (RadioButton)findViewById(R.id.radioButton);
-        rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        rb2 = (RadioButton)findViewById(R.id.radioButton2);
+        rb3 = (RadioButton)findViewById(R.id.radioButton3);
+        rb4 = (RadioButton)findViewById(R.id.radioButton4);
     }
 
     //Metodo para el boton calcular
@@ -43,6 +46,16 @@ public class MainActivity extends AppCompatActivity {
             int resta = valor1_Int - valor2_Int;
             String resultado = String.valueOf(resta);
             tvResultado.setText(resultado);
+        }else if (rb3.isChecked() == true){
+            int multiplicacion = valor1_Int * valor2_Int;
+            String resultado = String.valueOf(multiplicacion);
+            tvResultado.setText(resultado);
+        }else if (rb4.isChecked() == true && valor2_Int != 0){
+            int division = valor1_Int / valor2_Int;
+            String resultado = String.valueOf(division);
+            tvResultado.setText(resultado);
+        }else if (rb4.isChecked() == true && valor2_Int == 0){
+            Toast.makeText(this, "Como vas a dividir por 0 la concha de tu madre", Toast.LENGTH_LONG).show();
         }
     }
 }
